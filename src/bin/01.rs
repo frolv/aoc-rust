1d5ffbc174d5b5c105c626d6ea0223c236990d47
Alexei Frolov <frolv@google.com>
2022-12-01 13:01:30 -0800

    day01 #2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,22 +1,28 @@
+#![feature(binary_heap_into_iter_sorted)]
+
+use std::collections::BinaryHeap;
 use std::{env, fs};
 
 use anyhow::Result;
 
-fn main() -> Result<()> {
+fn max_n(n: usize) -> Result<i32> {
     let Some(path) = env::args().nth(1) else {
         anyhow::bail!("no file provided");
     };
 
-    let max = fs::read_to_string(path)?
+    let values: BinaryHeap<i32> = fs::read_to_string(path)?
         .split("\n\n")
         .map(|list| {
             list.split('\n')
                 .filter_map(|nums| nums.parse::<i32>().ok())
                 .sum::<i32>()
         })
-        .max()
-        .ok_or_else(|| anyhow::anyhow!("invalid input"))?;
+        .collect();
 
-    println!("max is {max}");
+    Ok(values.into_iter_sorted().take(n).sum())
+}
+
+fn main() -> Result<()> {
+    println!("max is {}", max_n(3)?);
     Ok(())
 }
